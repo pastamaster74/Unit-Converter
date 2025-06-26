36d005c3d1b3621014accad953e3b882ef9510f1
Pastamaster74 <aku.playstation@gmail.com>
2025-06-26 13:06:48 +0300

    more options added

--- UnitConverter.cpp
+++ UnitConverter.cpp
@@ -8,9 +8,9 @@ int main() {
         double amountToConvert; // Stores how much the user wants to convert of some unit
         double result; // Stores the result of the conversion
 
-        cout << "What do you want to convert/do? \n1. Meters to Feet \n2. Feet to Meters \n3. Exit" << endl;
+        cout << "What do you want to convert/do? \n1. Meters to Feet \n2. Feet to Meters \n3. Kilometers to Miles \n4. Miles to Kilometers \n5. Kilograms to Pounds \n6. Pounds to Kilograms \n7. Celcius to Fahrenheit \n8. Exit" << endl;
         cin >> input;
-        if (input == 3) {
+        if (input == 8) {
             exit = true;
         } else {
             cout << "Now enter how much you want to convert: ";
@@ -21,11 +21,20 @@ int main() {
                 result = amountToConvert * 3.2808;
             case 2:
                 result = amountToConvert * 0.3048;
+            case 3:
+                result = amountToConvert * 0.6213;
+            case 4:
+                result = amountToConvert * 1.6093;
+            case 5:
+                result = amountToConvert * 2.2046;
+            case 6:
+                result = amountToConvert * 0.4535;
+            case 7:
+                result = amountToConvert * 33.8;
             }
         
             cout << "Result: ";
             cout << result << endl;
-            cout << "Press ENTER to continue" << endl;
         }
     }
     return 0;
